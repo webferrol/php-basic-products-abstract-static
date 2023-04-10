0980aeed21e3998898b02a7e623b78a9fdd7099d
webferrol <xurxo@webferrol.com>
2023-04-10 19:45:41 +0200

    Refuerzo herencia, clases y métodos abstractos, static, datetime

---
+++ README.php
@@ -0,0 +1,16 @@
+<?php
+// Crear una clase Producto con tres campos: id, nombreProducto, precio. Este tendrá un método estático llamado "aplicarDescuento25" que recibirá un argumento que es el precio del producto y devolverá el precio del producto una vez aplicado el decuento del 25%
+
+
+
+// Crear una clase Alimento que herederará los campos anteriores. Se le añadirá un campo privado más de tipo boolean denominado tieneCaducidad y otro de tipo DateTime que insertemos la fecha de caducidad. Crear un método abstracto en la clase Producto del ejercicio anterior llamado dimeTipo en que se nos indicará el tipo de producto. En caso del alimento nos indicará si este producto tiene caducidad o no. En caso de tener caducidad indicar cuántos días faltan para su caducidad.
+
+
+// Crear otra clase Utensilio que también herederará de la clase Producto. Realizar las operaciones oportunas para su funcionamiento.
+
+
+// Crear una clase denominada Compra que tendrá como propiedades obligatorias un array de objetos Alimento y otro de clase Utensilio. Realizar todas las operaciones necesarias para que imprima un ticket con la compra total
+
+
+
+// Después de todos estos ejercicios realizar un formulario que nos permita el funcionamiento de la app y su listado.
